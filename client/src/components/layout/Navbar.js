34be0c12a4dfd43ecb57c97ac25c5799bfa32791
Asel Hidallearachchi <aselinuke@gmail.com>
2025-09-05 13:11:17 +0530

    Clean Up & Initial Components

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => (
+  <nav className="navbar bg-dark">
+    <h1>
+      <a href="/">
+        <i className="fas fa-code"></i> DevConnector
+      </a>
+    </h1>
+    <ul>
+      <li>
+        <a href="/profiles">Developers</a>
+      </li>
+      <li>
+        <a href="/register">Register</a>
+      </li>
+      <li>
+        <a href="/login">Login</a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
