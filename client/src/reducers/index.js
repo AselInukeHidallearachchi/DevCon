e00e1a308c830b96fbe679f744a837911a72885a
Asel Hidallearachchi <aselinuke@gmail.com>
2025-09-11 18:57:57 +0530

    Profile Reducer & Get Current Profile

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,16 +1,10 @@
-// src/reducers/index.js
 import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
-
-// Later you'll import real reducers like:
-// import profile from './profile';
-// import posts from './posts';
-// import alert from './alert';
+import profile from "./profile";
 
 export default combineReducers({
-  auth,
-  // profile,
-  // posts,
   alert,
+  auth,
+  profile,
 });
