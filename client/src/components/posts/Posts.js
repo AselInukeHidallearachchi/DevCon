b2a0ddcba5cc793baa5ced0e12703481c45f335e
Asel Hidallearachchi <aselinuke@gmail.com>
2025-09-15 00:53:53 +0530

    Post Reducer, Action & Initial Component

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,39 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { getPosts } from "../../actions/post";
+
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome to the community
+      </p>
+      {/* PostForm will go here */}
+      <div className="posts">
+        {posts.map((post) => (
+          <div key={post._id}>Post Item will go here</div>
+        ))}
+      </div>
+    </Fragment>
+  );
+};
+
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
