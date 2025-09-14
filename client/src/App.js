8da5789b5b5728e9dccc22e5caa94b83d5556747
Asel Hidallearachchi <aselinuke@gmail.com>
2025-09-15 02:48:00 +0530

    Not Found Page & Theme Workaround queue

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,9 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Alert from "./components/layout/Alert";
-import Dashboard from "./components/dashboard/Dashboard";
-import PrivateRoute from "./components/routing/PrivateRoute";
-import CreateProfile from "./components/profile-forms/CreateProfile";
-import EditProfile from "./components/profile-forms/EditProfile";
-import AddExperience from "./components/profile-forms/AddExperience";
-import AddEducation from "./components/profile-forms/AddEducation";
-import Profiles from "./components/profiles/Profiles";
-import Profile from "./components/profile/Profile";
-import Posts from "./components/posts/Posts";
-import Post from "./components/post/Post";
+import React, { useEffect } from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import CustomRoutes from './components/routing/Routes';
 
 ///Redux
 import store from "./store";
@@ -37,38 +25,10 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <div className="container">
-        <Alert />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profiles" element={<Profiles />} />
-          <Route path="/profile/:id" element={<Profile />} />
-          <Route
-            path="/dashboard"
-            element={<PrivateRoute component={Dashboard} />}
-          />
-          <Route
-            path="/create-profile"
-            element={<PrivateRoute component={CreateProfile} />}
-          />
-          <Route
-            path="/edit-profile"
-            element={<PrivateRoute component={EditProfile} />}
-          />
-          <Route
-            path="/add-experience"
-            element={<PrivateRoute component={AddExperience} />}
-          />
-          <Route
-            path="/add-education"
-            element={<PrivateRoute component={AddEducation} />}
-          />
-          <Route path="/posts" element={<PrivateRoute component={Posts} />} />
-          <Route path="/post/:id" element={<PrivateRoute component={Post} />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route path="/*" element={<CustomRoutes />} />
+      </Routes>
     </>
   );
 };
