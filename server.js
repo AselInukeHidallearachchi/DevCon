951fe8040ade68c6caf0455f759c6074949a9ad8
Asel Hidallearachchi <92600731+AselInukeHidallearachchi@users.noreply.github.com>
2025-09-16 05:40:28 +0530

    Fix string quotes and static file serving

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
-const path = require('path');
+const path = require("path");
 const cors = require("cors");
 
 const app = express();
@@ -19,15 +19,14 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
-app.use("/api/password", require("./routes/api/password"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
